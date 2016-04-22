d3e8fcec4f4ac85c7b0bc47d9aecab0b5ab450c7
Brian Lane <brian@gourmetgaming.com>
2016-04-22 14:35:02 -0400

    Updated Sprite Sheet with a font copied from the web, and created the font rendering method in the Font class

---
+++ 2DGameSeries/src/com/bwlane/game/gfx/Font.java
@@ -0,0 +1,20 @@
+package com.bwlane.game.gfx;
+
+public class Font {
+	
+	private static String chars = 
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ      " +
+			"01234567789.,:;'\"!?$%()-=+/      ";
+	
+	public static void render(String msg, Screen screen, int x, int y, int color)
+	{
+		msg = msg.toUpperCase();
+		
+		for (int i = 0; i < msg.length(); i++) {
+			int charIndex = chars.indexOf(msg.charAt(i));
+			if (charIndex >= 0) {
+				screen.render(x + (i*8), y, charIndex + 30 * 32, color);
+			}
+		}
+	}
+}
